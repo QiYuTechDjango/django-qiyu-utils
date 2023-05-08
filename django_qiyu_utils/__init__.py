98a053b058b8fad3f45b54aa216bcf0977a82fdd
dev <dev@qiyutech.tech>
2023-05-09 00:19:40 +0800

    * 删除无用的代码 * 升级版本为 0.5.0

--- django_qiyu_utils/__init__.py
+++ django_qiyu_utils/__init__.py
@@ -1,5 +1,4 @@
 from .env import EnvHelper
-from .paginator import CustomPaginator  # noqa
 from .redirect import RedirectHelper
 
-__all__ = ["RedirectHelper", "EnvHelper", "CustomPaginator"]
+__all__ = ["RedirectHelper", "EnvHelper"]
